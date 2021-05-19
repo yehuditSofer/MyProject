24a865059632aed0f24e45d8c855c6e304877577
yehuditSofer <84419115+yehuditSofer@users.noreply.github.com>
2021-05-19 08:45:23 +0300

    Update line-donation.component.ts

--- src/app/line-donation/line-donation.component.ts
+++ src/app/line-donation/line-donation.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-donation.component.css']
 })
 export class LineDonationComponent implements OnInit {
-
+  obj1 = false;
+  text1 = 'יישות 1';
+  text2 = '100 ש""ח';
   constructor() { }
 
   ngOnInit(): void {
+    
   }
-
+  changObj():void {
+    this.obj1 = (this.obj1!)
+  }
 }
