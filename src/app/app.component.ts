8b58ff9ce6383d775686b571984d8479c4fbd826
yehuditSofer <84419115+yehuditSofer@users.noreply.github.com>
2021-05-19 08:33:44 +0300

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DonationService } from './donation.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyProject';
+  obj1 = false;
+  text1 = 'יישות 1';
+  text2 = '100 ש""ח';
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  changObj():void {
+    this.obj1 = (this.obj1!)
+  }
 }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from '@angular/common/http';
+import { LineDonationComponent } from './line-donation/line-donation.component';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+
+const appRoutes: Routes = [
+  { path: 'LineDonation', component: LineDonationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineDonationComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
